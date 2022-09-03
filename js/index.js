25a3332db3db20f7e5bb5ef40362d044d83cc0c4
nayeem-haque <nayeemhaque43@gmail.com>
2022-09-04 00:47:48 +0600

    Second commit

--- js/index.js
+++ js/index.js
@@ -34,22 +34,18 @@ const displayCategoryNews = (news) => {
          <img src="${allNews.image_url}" alt="" width="600" height="400">
          </div>
           <h5 class="card-title">${allNews.title}</h5>
-          <p>${
-            allNews.details.length > 100
-              ? allNews.details.slice(0, 100) + "..."
-              : allNews.details
-          }</p>
+          <p>${allNews.details.length > 100
+        ? allNews.details.slice(0, 100) + "..."
+        : allNews.details
+      }</p>
           <div class="flex">
-            <div><img src="${
-              allNews.author.img
-            }" alt="" width="50" height="30" style="border-radius: 50%;"></div> &nbsp; &nbsp;
-            <div><span style="color:purple;font-weight:bold;">${
-              allNews.author.name
-            }</span><br>
+            <div><img src="${allNews.author.img
+      }" alt="" width="50" height="30" style="border-radius: 50%;"></div> &nbsp; &nbsp;
+            <div><span style="color:purple;font-weight:bold;">${allNews.author.name
+      }</span><br>
             <span>${allNews.author.published_date}</span>
-            <span style="font-weight:bold;">&nbsp;&nbsp;&nbsp; Views: ${
-              allNews.total_view
-            }</span>
+            <span style="font-weight:bold;">&nbsp;&nbsp;&nbsp; Views: ${allNews.total_view
+      }</span>
             <span style="color:blue;font-weight:bold;">&nbsp;&nbsp;&nbsp; -></span></div>
           </div>
         </div>
